cda7cd88be4115d1f2b38f7605e07ae396e32125
Zita <44496590+he201525@users.noreply.github.com>
2018-12-08 15:06:28 +0100

    Update welcomePage.java

--- ZeroDechetV2/src/View/welcomePage.java
+++ ZeroDechetV2/src/View/welcomePage.java
@@ -1,22 +1,18 @@
-package Projet;
-
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
-import java.awt.SystemColor;
-import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+
 import java.awt.Font;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class welcomePage extends JFrame {
 
-	private JPanel connecteBtn;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -38,48 +34,22 @@ public class welcomePage extends JFrame {
 	 * Create the frame.
 	 */
 	public welcomePage() {
-		setBackground(SystemColor.desktop);
+		setForeground(new Color(102, 102, 102));
+		setBackground(new Color(255, 255, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 981, 491);
-		connecteBtn = new JPanel();
-		connecteBtn.setBackground(SystemColor.textHighlight);
-		connecteBtn.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(connecteBtn);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 153, 255));
+		contentPane.setForeground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 		
-		JLabel vBienvenue = new JLabel("Bienvenue \u00E0 toi \r\ncher d\u00E9fenseur \r\r\nde la nature \r\r\n#ZeroD\u00E9chet");
-		vBienvenue.setForeground(Color.LIGHT_GRAY);
-		vBienvenue.setBackground(SystemColor.window);
-		vBienvenue.setFont(new Font("Century Gothic", Font.PLAIN, 30));
-		
-		JButton ConnecteToiBtn = new JButton("Connecte toi :");
-		
-		JButton InscritToiBtn = new JButton("Inscrit toi :");
-		GroupLayout gl_connecteBtn = new GroupLayout(connecteBtn);
-		gl_connecteBtn.setHorizontalGroup(
-			gl_connecteBtn.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_connecteBtn.createSequentialGroup()
-					.addGroup(gl_connecteBtn.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_connecteBtn.createSequentialGroup()
-							.addGap(307)
-							.addGroup(gl_connecteBtn.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(InscritToiBtn, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(ConnecteToiBtn, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)))
-						.addGroup(gl_connecteBtn.createSequentialGroup()
-							.addGap(104)
-							.addComponent(vBienvenue, GroupLayout.PREFERRED_SIZE, 701, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(148, Short.MAX_VALUE))
-		);
-		gl_connecteBtn.setVerticalGroup(
-			gl_connecteBtn.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_connecteBtn.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(vBienvenue, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 163, Short.MAX_VALUE)
-					.addComponent(ConnecteToiBtn)
-					.addGap(18)
-					.addComponent(InscritToiBtn)
-					.addGap(61))
-		);
-		connecteBtn.setLayout(gl_connecteBtn);
+		JTextPane Bienvenue = new JTextPane();
+		Bienvenue.setFont(new Font("Dialog", Font.PLAIN, 13));
+		Bienvenue.setText("Bienvenue à toi\n cher defenseur\n de la nature\n #zéroDéchet");
+		contentPane.add(Bienvenue, BorderLayout.CENTER);
+	
 	}
+
 }

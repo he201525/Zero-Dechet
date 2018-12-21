861db5b69d17276e2dcbec56abac96fc610c16c1
sebfoothock <32933260+sebfoothock@users.noreply.github.com>
2018-12-21 16:47:13 +0100

    Update SignIn.java

--- ZeroDechetV3/src/view/SignIn.java
+++ ZeroDechetV3/src/view/SignIn.java
@@ -1,8 +1,12 @@
+package view;
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
+
+import model.Joueur;
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -11,41 +15,66 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.SwingConstants;
+import java.util.Observable;
+import java.util.Observer;
 
-
-public class SignIn extends JFrame {
+/*
+ * Cette classe creer une JFrame qui permet à l'utilisateur de se connecter à l'application.
+ * Groupe 16
+ * @author Sébastien Raemdonck
+ * @author Aymeric Ponjée
+ * @author Zita Almasy
+ */
+public class SignIn extends JFrame  implements ActionListener, Observer{
 
 	private JPanel contentPane;
 	private JTextField PseudotextField;
 	private JTextField MotdePasseField;
+	private JButton InscrireButton;
+	private JLabel PseudoLBL;
+	private JLabel MotdePasseLBL;
+	
+	
+	static SignIn home ;
+	
+	LvlPlanet level = new LvlPlanet();
+	Joueur player = new Joueur();
+	static SignIn frame;
+	
 
 	
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void launch() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					SignIn frame = new SignIn();
+					frame = new SignIn();
 					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					System.out.println("erreur");;
 				}
 			}
 		});
 	}
+	
+	public void closeSignIn() {
+		this.dispose();
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public SignIn() {
-		setTitle("Z\u00E9roD\u00E9chet");
-		setResizable(false);
+		
+		
+		
+		
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 410, 500);
+		setBounds(100, 100, 410, 450);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(240, 248, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -53,61 +82,58 @@ public class SignIn extends JFrame {
 		contentPane.setLayout(null);
 		
 			
-			JLabel connectionLBL = new JLabel("CONNECTION");
-			connectionLBL.setForeground(new Color(0, 0, 128));
-	        connectionLBL.setFont(new Font("Century Gothic", Font.BOLD, 20));
-	        connectionLBL.setBounds(125, 95, 157, 49);
-	        contentPane.add(connectionLBL);
-		
-	        JButton InscrireButton = new JButton("GO! ");
-	        InscrireButton.setBackground(new Color(0, 0, 128));
-	        InscrireButton.setForeground(Color.WHITE);
-	        InscrireButton.setFont(new Font("Century Gothic", Font.BOLD, 13));
-	        InscrireButton.setBounds(154, 383, 117, 38);
-	        contentPane.add(InscrireButton);
+			InscrireButton = new JButton("GO! ");
+			InscrireButton.setBackground(new Color(240, 248, 255));
+			InscrireButton.setForeground(new Color(0, 153, 255));
+			InscrireButton.setFont(new Font("Century Gothic", Font.PLAIN, 13));
+			InscrireButton.setBounds(269, 354, 117, 29);
+			contentPane.add(InscrireButton);
 			
-	        PseudotextField = new JTextField();
-	        PseudotextField.setForeground(new Color(0, 0, 128));
-	        PseudotextField.setFont(new Font("Century Gothic", Font.PLAIN, 15));
-	        PseudotextField.setBounds(125, 190, 176, 38);
-	        contentPane.add(PseudotextField);
-	        PseudotextField.setColumns(10);
-	        
-	        JLabel Erreur = new JLabel("Erreur");
-	        Erreur.setHorizontalAlignment(SwingConstants.CENTER);
-	        Erreur.setFont(new Font("Century Gothic", Font.PLAIN, 13));
-	        Erreur.setForeground(Color.RED);
-	        Erreur.setBounds(125, 43, 176, 39);
-	        contentPane.add(Erreur);
+			PseudotextField = new JTextField();
+			PseudotextField.setBounds(124, 165, 176, 26);
+			contentPane.add(PseudotextField);
+			PseudotextField.setColumns(10);
 			
-	        JLabel PseudoLBL = new JLabel("Pseudo");
-	        PseudoLBL.setForeground(Color.WHITE);
-	        PseudoLBL.setFont(new Font("Century Gothic", Font.BOLD, 19));
-	        PseudoLBL.setBounds(125, 160, 89, 29);
-	        contentPane.add(PseudoLBL);
+			PseudoLBL = new JLabel("Pseudo");
+			PseudoLBL.setForeground(Color.WHITE);
+			PseudoLBL.setFont(new Font("Century Gothic", Font.BOLD, 15));
+			PseudoLBL.setBounds(124, 145, 61, 16);
+			contentPane.add(PseudoLBL);
 			
-	        JLabel MotdePasseLBL = new JLabel("Mot de Passe");
-	        MotdePasseLBL.setForeground(Color.WHITE);
-	        MotdePasseLBL.setFont(new Font("Century Gothic", Font.BOLD, 19));
-	        MotdePasseLBL.setBounds(125, 250, 192, 36);
-	        contentPane.add(MotdePasseLBL);
+			MotdePasseLBL = new JLabel("Mot de Passe");
+			MotdePasseLBL.setForeground(Color.WHITE);
+			MotdePasseLBL.setFont(new Font("Century Gothic", Font.BOLD, 15));
+			MotdePasseLBL.setBounds(124, 220, 130, 16);
+			contentPane.add(MotdePasseLBL);
 			
 			MotdePasseField = new JPasswordField();
 			MotdePasseField.setColumns(10);
-			MotdePasseField.setBounds(125, 280, 176, 38);
-			MotdePasseField.setForeground(new Color(0, 0, 128));
+			MotdePasseField.setBounds(124, 240, 176, 26);
 			contentPane.add(MotdePasseField);
-			/*
-			InscrireButton.addActionListener(new ActionListener() {
-				public void actionPerformed(249, -11ent e) {
-					
-					
-				}
-			});
-			*/
+			
+			
+			InscrireButton.addActionListener(this); 
+				
+			
 			JLabel bg1 = new JLabel(); // initialization of JLabel
-			bg1.setIcon(new ImageIcon("C:\\Users\\Aymeric\\Documents\\Ephec\\bac 2\\Java\\ZeroDechet\\Img\\b1111bis.png"));
-			bg1.setBounds(-141, -14, 699, 513); 
+			bg1.setIcon(new ImageIcon("C:\\Users\\Republic\\Desktop\\JAVA\\b1111bis.png"));
+			bg1.setBounds(-225, -46, 699, 513); 
 			contentPane.add(bg1);// adding JLabel to content pane
 	}
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == InscrireButton) {
+			if(player.Connexion(PseudotextField.getText(), MotdePasseField.getText())) {
+				closeSignIn();
+				ViewLevel.launch(1);
+			}
+		}
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		frame.pack();
+	}
+	
+	
 }

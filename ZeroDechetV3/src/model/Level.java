92b8096cbe6f4c356bd8a20e70a5c423a8116e0e
sebfoothock <32933260+sebfoothock@users.noreply.github.com>
2018-12-21 16:50:16 +0100

    Update Level.java

--- ZeroDechetV3/src/model/Level.java
+++ ZeroDechetV3/src/model/Level.java
@@ -1,40 +1,130 @@
 package model;
 
-import java.util.ArrayList;
-import model.Mission;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /*
- * Cette classe permet d'afficher les missions.
+ * Cette classe va permettre de définir une mission qui apparaître dans la console et la GUI.
  * Groupe 16
  * @author Sébastien Raemdonck
  * @author Aymeric Ponjée
  * @author Zita Almasy
  */
-public class Level {
-	private ArrayList <Mission> missions;
+
+public class Mission {
 	
-	public Level(int niveau) {
-		this.missions = new ArrayList<Mission>();
-		int i = 0;
-		while(i != 3) {
-			Mission mission1 = new Mission(niveau);
-			if (!missions.contains(mission1.getContenu())) {//vérifie qui ne contient pas déjà la mission 
-				missions.add(mission1);//contains fonction comme equals
-				//peut renvoyer 2 fois même question
-				i++;
+	private static String url = "jdbc:mysql://localhost/sys?useTimerzone=true&serverTimezone=UTC";
+	private static String user = "root";
+	private static String pwd = "root";
+	
+	private Connection myConnection = null;//pas de static car indépendante à chaque mission
+	private Statement myState = null;
+	private ResultSet myResult = null;
+	
+	private String sql = "";
+	private String driver;
+
+	private String contenu;//question
+	private int level;//le niveau de la mission
+	private int expMission;
+	private int idMission; 
+	
+	//private Etat etat;//etat de la mission si elle est réussi, attente ou impossible
+	
+
+
+	/**
+	 * Ce constructeur permet de définir le niveau de la mission
+	 * @param level Niveau de la mission
+	 */
+	
+	public Mission(int level) {
+		this.level = level;
+		//System.out.println(this.level);
+		try {
+			Connexion();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	/**
+	 * Cette méthode permet la connexion à la base de donnée pour récupérer une mission 
+	 *
+	 */
+	public Object Connexion (){
+		try {
+			
+			this.driver = "com.mysql.cj.jdbc.Driver";
+			this.myConnection = DriverManager.getConnection(this.url, this.user, this.pwd);
+			this.myState = myConnection.createStatement();
+			this.sql = "";
+			this.myResult = myState.executeQuery("select * from mission where idMission = " + level + " ORDER BY RAND() limit 1");
+			
+			
+			//obliger d'utiliser un while avec la valeur du niveau pour reprendre les données de la ligne dans la DB
+			while(this.myResult.next()) {
+			this.contenu = this.myResult.getString("contenu");
+			this.expMission = this.myResult.getInt("expMission");
+			this.idMission = this.myResult.getInt("idMission");
+		
+			}
+			
+		}
+		catch (SQLException e) {
+			System.out.println("erreur connexion(Mission)");
+		}
+		
+		finally {
+			try {
+				 myState.close();
+				 myConnection.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
 			}
 		}
 		
-	}
-		
-	public ArrayList<Mission> getMissions() {
-		return missions;
+		return null;
+}
+	//Getter and Setter
+	public int getIdMission() {
+		return idMission;
 	}
 
-
-	public void setMissions(ArrayList<Mission> missions) {
-		this.missions = missions;
+	public void setIdMission(int idMission) {
+		this.idMission = idMission;
 	}
+
+	public int getExp() {
+		return expMission;
+	}
+
+	public void setExp(int exp) {
+		this.expMission = exp;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
 }
 
